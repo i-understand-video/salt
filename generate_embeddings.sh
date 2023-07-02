31860e29e1144e0d02b84d7b9f3a904c6c9851c3
metric-space <functor.soup@gmail.com>
2023-07-02 16:35:47 -0400

    Freeze local pip installations and add bash scripys

---
+++ generate_embeddings.sh
@@ -0,0 +1,1 @@
+python helpers/extract_embeddings.py --checkpoint-path MODELS/sam_vit_h_4b8939.pth  --dataset-path pictures
---
+++ generate_onnx.sh
@@ -0,0 +1,1 @@
+python helpers/generate_onnx.py --checkpoint-path ./MODELS/sam_vit_h_4b8939.pth --dataset-path pictures
